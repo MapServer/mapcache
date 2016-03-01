507d169cb9ca65ef0b9e751eb1a96c40f0907062
Thomas Bonfort <thomas.bonfort@gmail.com>
2016-03-01 18:35:41 +0100

    Re-enable and fix GDAL source

--- lib/source_gdal.c
+++ lib/source_gdal.c
@@ -54,180 +54,212 @@ struct mapcache_source_gdal {
   GDALDatasetH *poDataset;
 };
 
+/************************************************************************/
+/*                        GDALWarpCreateOutput()                        */
+/*                                                                      */
+/*      Create the output file based on various commandline options,    */
+/*      and the input file.                                             */
+/************************************************************************/
+
+static GDALDatasetH
+GDALWarpCreateOutput( GDALDatasetH hSrcDS, const char *pszSourceSRS, const char *pszTargetSRS,
+                      int width, int height, mapcache_extent *extent)
+{
+    GDALDriverH hDriver;
+    GDALDatasetH hDstDS;
+    void *hTransformArg;
+    double adfDstGeoTransform[6];
+    double res_x, res_y;
+    int nPixels,nLines;
+
+    hDriver = GDALGetDriverByName( "MEM" );
+
+/* -------------------------------------------------------------------- */
+/*      Create a transformation object from the source to               */
+/*      destination coordinate system.                                  */
+/* -------------------------------------------------------------------- */
+    hTransformArg =
+        GDALCreateGenImgProjTransformer( hSrcDS, pszSourceSRS,
+                                         NULL, pszTargetSRS,
+                                         TRUE, 1000.0, 0 );
+
+    if( hTransformArg == NULL )
+        return NULL;
+
+/* -------------------------------------------------------------------- */
+/*      Get approximate output definition.                              */
+/* -------------------------------------------------------------------- */
+    if( GDALSuggestedWarpOutput( hSrcDS,
+                                 GDALGenImgProjTransform, hTransformArg,
+                                 adfDstGeoTransform, &nPixels, &nLines )
+        != CE_None )
+        return NULL;
+
+    GDALDestroyGenImgProjTransformer( hTransformArg );
+
+    res_x = (extent->maxx - extent->minx) / width;
+    res_y = (extent->maxy - extent->miny) / height;
+
+    adfDstGeoTransform[0] = extent->minx;
+    adfDstGeoTransform[3] = extent->maxy;
+    adfDstGeoTransform[1] = res_x;
+    adfDstGeoTransform[5] = -res_y;
+
+
+    hDstDS = GDALCreate( hDriver, "temp_gdal", width, height, 4, GDT_Byte, NULL);
+
+    if( hDstDS == NULL )
+        return NULL;
+
+/* -------------------------------------------------------------------- */
+/*      Write out the projection definition.                            */
+/* -------------------------------------------------------------------- */
+    GDALSetProjection( hDstDS, pszTargetSRS );
+    GDALSetGeoTransform( hDstDS, adfDstGeoTransform );
+
+    return hDstDS;
+}
 
 /**
  * \private \memberof mapcache_source_gdal
  * \sa mapcache_source::render_metatile()
  */
-void _mapcache_source_gdal_render_metatile(mapcache_context *ctx, mapcache_metatile *tile)
+void _mapcache_source_gdal_render_metatile(mapcache_context *ctx, mapcache_map *map)
 {
-  mapcache_source_gdal *gdal = (mapcache_source_gdal*)tile->tile.tileset->source;
-  char *srcSRS = "", *dstSRS;
-  mapcache_buffer *data = mapcache_buffer_create(0,ctx->pool);
-  GC_CHECK_ERROR(ctx);
-  GDALDatasetH  hDataset;
+  mapcache_source_gdal *gdal = (mapcache_source_gdal*)map->tileset->source;
+  GDALDatasetH  hSrcDS,hDstDS;
+  OGRSpatialReferenceH hDstSRS;
+  char *src_srs,*dst_srs;
+  mapcache_buffer *data;
+  char **papszWarpOptions = NULL;
+  void *hTransformArg, *hGenImgProjArg=NULL;
+  GDALTransformerFunc pfnTransformer = NULL;
+  GDALRasterBandH *redband, *greenband, *blueband, *alphaband;
+  unsigned char *rasterdata;
 
-  GDALAllRegister();
-  OGRSpatialReferenceH hSRS;
   CPLErrorReset();
 
-  hSRS = OSRNewSpatialReference( NULL );
-  if( OSRSetFromUserInput( hSRS, tile->tile.grid->srs ) == OGRERR_NONE )
-    OSRExportToWkt( hSRS, &dstSRS );
-  else {
-    ctx->set_error(ctx,MAPCACHE_SOURCE_GDAL_ERROR,"failed to parse gdal srs %s",tile->tile.grid->srs);
-    return;
-  }
+  /* -------------------------------------------------------------------- */
+  /*      Open source dataset.                                            */
+  /* -------------------------------------------------------------------- */
+  hSrcDS = GDALOpen( gdal->datastr, GA_ReadOnly );
 
-  OSRDestroySpatialReference( hSRS );
-
-  hDataset = GDALOpen( gdal->datastr, GA_ReadOnly );
-  if( hDataset == NULL ) {
-    ctx->set_error(ctx,MAPCACHE_SOURCE_GDAL_ERROR,"GDAL failed to open %s",gdal->datastr);
-    return;
-  }
+  if( hSrcDS == NULL )
+    exit( 2 );
 
   /* -------------------------------------------------------------------- */
   /*      Check that there's at least one raster band                     */
   /* -------------------------------------------------------------------- */
-  if ( GDALGetRasterCount(hDataset) == 0 ) {
-    ctx->set_error(ctx,MAPCACHE_SOURCE_GDAL_ERROR,"raster %s has no bands",gdal->datastr);
+  if ( GDALGetRasterCount(hSrcDS) == 0 ) {
+    ctx->set_error(ctx, 500, "Input gdal source for %s has no raster bands.\n", gdal->source.name );
+    GDALClose(hSrcDS);
     return;
   }
 
-  if( GDALGetProjectionRef( hDataset ) != NULL
-      && strlen(GDALGetProjectionRef( hDataset )) > 0 )
-    srcSRS = apr_pstrdup(ctx->pool,GDALGetProjectionRef( hDataset ));
-
-  else if( GDALGetGCPProjection( hDataset ) != NULL
-           && strlen(GDALGetGCPProjection(hDataset)) > 0
-           && GDALGetGCPCount( hDataset ) > 1 )
-    srcSRS = apr_pstrdup(ctx->pool,GDALGetGCPProjection( hDataset ));
-
-  GDALDriverH hDriver = GDALGetDriverByName( "MEM" );
-  GDALDatasetH hDstDS;
-  /* -------------------------------------------------------------------- */
-  /*      Create a transformation object from the source to               */
-  /*      destination coordinate system.                                  */
-  /* -------------------------------------------------------------------- */
-  void *hTransformArg =
-    GDALCreateGenImgProjTransformer( hDataset, srcSRS,
-                                     NULL, dstSRS,
-                                     TRUE, 1000.0, 0 );
-
-  if( hTransformArg == NULL ) {
-    ctx->set_error(ctx,MAPCACHE_SOURCE_GDAL_ERROR,"gdal failed to create SRS transformation object");
+  if( GDALGetProjectionRef( hSrcDS ) != NULL
+      && strlen(GDALGetProjectionRef( hSrcDS )) > 0 ) {
+    src_srs = apr_pstrdup(ctx->pool,GDALGetProjectionRef( hSrcDS ));
+  } else if( GDALGetGCPProjection( hSrcDS ) != NULL
+           && strlen(GDALGetGCPProjection(hSrcDS)) > 0
+           && GDALGetGCPCount( hSrcDS ) > 1 ) {
+    src_srs = apr_pstrdup(ctx->pool,GDALGetGCPProjection( hSrcDS ));
+  } else {
+    ctx->set_error(ctx, 500, "Input gdal source for %s has no defined SRS\n", gdal->source.name );
+    GDALClose(hSrcDS);
     return;
   }
 
-  /* -------------------------------------------------------------------- */
-  /*      Get approximate output definition.                              */
-  /* -------------------------------------------------------------------- */
-  int nPixels, nLines;
-  double adfDstGeoTransform[6];
-  if( GDALSuggestedWarpOutput( hDataset,
-                               GDALGenImgProjTransform, hTransformArg,
-                               adfDstGeoTransform, &nPixels, &nLines )
-      != CE_None ) {
-    ctx->set_error(ctx,MAPCACHE_SOURCE_GDAL_ERROR,"gdal failed to create suggested warp output");
+
+  hDstSRS = OSRNewSpatialReference( NULL );
+  if( OSRSetFromUserInput( hDstSRS, map->grid_link->grid->srs ) == OGRERR_NONE )
+    OSRExportToWkt( hDstSRS, &dst_srs );
+  else {
+    ctx->set_error(ctx,500,"failed to parse gdal srs %s",map->grid_link->grid->srs);
     return;
   }
 
-  GDALDestroyGenImgProjTransformer( hTransformArg );
-  double dfXRes = (tile->bbox[2] - tile->bbox[0]) / tile->sx;
-  double dfYRes = (tile->bbox[3] - tile->bbox[1]) / tile->sy;
+  OSRDestroySpatialReference( hDstSRS );
 
-  adfDstGeoTransform[0] = tile->bbox[0];
-  adfDstGeoTransform[3] = tile->bbox[3];
-  adfDstGeoTransform[1] = dfXRes;
-  adfDstGeoTransform[5] = -dfYRes;
-  hDstDS = GDALCreate( hDriver, "tempd_gdal_image", tile->sx, tile->sy, 4, GDT_Byte, NULL );
-
-  /* -------------------------------------------------------------------- */
-  /*      Write out the projection definition.                            */
-  /* -------------------------------------------------------------------- */
-  GDALSetProjection( hDstDS, dstSRS );
-  GDALSetGeoTransform( hDstDS, adfDstGeoTransform );
-  char               **papszWarpOptions = NULL;
+  hDstDS = GDALWarpCreateOutput( hSrcDS, src_srs, dst_srs, map->width, map->height, &map->extent);
   papszWarpOptions = CSLSetNameValue( papszWarpOptions, "INIT", "0" );
 
+/* -------------------------------------------------------------------- */
+/*      Create a transformation object from the source to               */
+/*      destination coordinate system.                                  */
+/* -------------------------------------------------------------------- */
+  hGenImgProjArg = GDALCreateGenImgProjTransformer( hSrcDS, src_srs,
+                                                    hDstDS, dst_srs,
+                                                    TRUE, 1000.0, 0 );
 
+  if( hGenImgProjArg == NULL ) {
+    ctx->set_error(ctx,500,"failed to GDALCreateGenImgProjTransformer()");
+    return;
+  }
 
-  /* -------------------------------------------------------------------- */
-  /*      Create a transformation object from the source to               */
-  /*      destination coordinate system.                                  */
-  /* -------------------------------------------------------------------- */
-  GDALTransformerFunc pfnTransformer = NULL;
-  void               *hGenImgProjArg=NULL, *hApproxArg=NULL;
-  hTransformArg = hGenImgProjArg =
-                    GDALCreateGenImgProjTransformer( hDataset, srcSRS,
-                        hDstDS, dstSRS,
-                        TRUE, 1000.0, 0 );
-
-  if( hTransformArg == NULL )
-    exit( 1 );
-
-  pfnTransformer = GDALGenImgProjTransform;
-
-  hTransformArg = hApproxArg =
-                    GDALCreateApproxTransformer( GDALGenImgProjTransform,
-                        hGenImgProjArg, 0.125 );
+/* -------------------------------------------------------------------- */
+/*      Warp the transformer with a linear approximator                 */
+/* -------------------------------------------------------------------- */
+  hTransformArg = GDALCreateApproxTransformer( GDALGenImgProjTransform,
+                                               hGenImgProjArg, 0.1 );
   pfnTransformer = GDALApproxTransform;
 
   /* -------------------------------------------------------------------- */
   /*      Now actually invoke the warper to do the work.                  */
   /* -------------------------------------------------------------------- */
-  GDALSimpleImageWarp( hDataset, hDstDS, 0, NULL,
+  GDALSimpleImageWarp( hSrcDS, hDstDS, 0, NULL,
                        pfnTransformer, hTransformArg,
                        GDALDummyProgress, NULL, papszWarpOptions );
 
+
   CSLDestroy( papszWarpOptions );
 
-  if( hApproxArg != NULL )
-    GDALDestroyApproxTransformer( hApproxArg );
+  GDALDestroyApproxTransformer( hTransformArg );
+  GDALDestroyGenImgProjTransformer( hGenImgProjArg );
 
-  if( hGenImgProjArg != NULL )
-    GDALDestroyGenImgProjTransformer( hGenImgProjArg );
-
-  if(GDALGetRasterCount(hDstDS) != 4) {
-    ctx->set_error(ctx,MAPCACHE_SOURCE_GDAL_ERROR,"gdal did not create a 4 band image");
+  if(GDALGetRasterCount(hDstDS) < 3) {
+    ctx->set_error(ctx, 500,"gdal did not create a 3/4 band image");
     return;
   }
 
-  GDALRasterBandH *redband, *greenband, *blueband, *alphaband;
 
   redband = GDALGetRasterBand(hDstDS,1);
   greenband = GDALGetRasterBand(hDstDS,2);
   blueband = GDALGetRasterBand(hDstDS,3);
-  alphaband = GDALGetRasterBand(hDstDS,4);
+  if(GDALGetRasterCount(hDstDS) > 3) {
+    alphaband = GDALGetRasterBand(hDstDS,4);
+  } else {
+    alphaband = NULL;
+  }
 
-  unsigned char *rasterdata = apr_palloc(ctx->pool,tile->sx*tile->sy*4);
-  data->buf = rasterdata;
-  data->avail = tile->sx*tile->sy*4;
-  data->size = tile->sx*tile->sy*4;
+  data = mapcache_buffer_create(map->height*map->width*4,ctx->pool);
+  rasterdata = data->buf;
 
-  GDALRasterIO(redband,GF_Read,0,0,tile->sx,tile->sy,(void*)(rasterdata),tile->sx,tile->sy,GDT_Byte,4,4*tile->sx);
-  GDALRasterIO(greenband,GF_Read,0,0,tile->sx,tile->sy,(void*)(rasterdata+1),tile->sx,tile->sy,GDT_Byte,4,4*tile->sx);
-  GDALRasterIO(blueband,GF_Read,0,0,tile->sx,tile->sy,(void*)(rasterdata+2),tile->sx,tile->sy,GDT_Byte,4,4*tile->sx);
-  if(GDALGetRasterCount(hDataset)==4)
-    GDALRasterIO(alphaband,GF_Read,0,0,tile->sx,tile->sy,(void*)(rasterdata+3),tile->sx,tile->sy,GDT_Byte,4,4*tile->sx);
+  GDALRasterIO(blueband,GF_Read,0,0,map->width,map->height, rasterdata,map->width,map->height,GDT_Byte,4,4*map->width);
+  GDALRasterIO(greenband,GF_Read,0,0,map->width,map->height, rasterdata+1,map->width, map->height,GDT_Byte,4,4*map->width);
+  GDALRasterIO(redband,GF_Read,0,0,map->width,map->height,rasterdata+2,map->width,map->height,GDT_Byte,4,4*map->width);
+  if(GDALGetRasterCount(hDstDS)>=4)
+    GDALRasterIO(alphaband,GF_Read,0,0,map->width,map->height,rasterdata+3,map->width,map->height,GDT_Byte,4,4*map->width);
   else {
     unsigned char *alphaptr;
     int i;
-    for(alphaptr = rasterdata+3, i=0; i<tile->sx*tile->sy; i++, alphaptr+=4) {
+    for(alphaptr = rasterdata+3, i=0; i<map->width*map->height; i++, alphaptr+=4) {
       *alphaptr = 255;
     }
   }
 
-  tile->imdata = mapcache_image_create(ctx);
-  tile->imdata->w = tile->sx;
-  tile->imdata->h = tile->sy;
-  tile->imdata->stride = tile->sx * 4;
-  tile->imdata->data = rasterdata;
+  map->raw_image = mapcache_image_create(ctx);
+  map->raw_image->w = map->width;
+  map->raw_image->h = map->height;
+  map->raw_image->stride = map->width * 4;
+  map->raw_image->data = rasterdata;
+  if(alphaband)
+    map->raw_image->has_alpha = MC_ALPHA_UNKNOWN;
+  else
+    map->raw_image->has_alpha = MC_ALPHA_NO;
 
 
   GDALClose( hDstDS );
-  GDALClose( hDataset);
+  GDALClose( hSrcDS);
 }
 
 /**
@@ -260,12 +292,12 @@ void _mapcache_source_gdal_configuration_check(mapcache_context *ctx, mapcache_c
   mapcache_source_gdal *src = (mapcache_source_gdal*)source;
   /* check all required parameters are configured */
   if(!strlen(src->datastr)) {
-    ctx->set_error(ctx, MAPCACHE_SOURCE_GDAL_ERROR, "gdal source %s has no data",source->name);
+    ctx->set_error(ctx, 500, "gdal source %s has no data",source->name);
     return;
   }
   src->poDataset = (GDALDatasetH*)GDALOpen(src->datastr,GA_ReadOnly);
   if( src->poDataset == NULL ) {
-    ctx->set_error(ctx, MAPCACHE_SOURCE_GDAL_ERROR, "gdalOpen failed on data %s", src->datastr);
+    ctx->set_error(ctx, 500, "gdalOpen failed on data %s", src->datastr);
     return;
   }
 
@@ -275,18 +307,18 @@ void _mapcache_source_gdal_configuration_check(mapcache_context *ctx, mapcache_c
 mapcache_source* mapcache_source_gdal_create(mapcache_context *ctx)
 {
 #ifdef USE_GDAL
-  GDALAllRegister();
   mapcache_source_gdal *source = apr_pcalloc(ctx->pool, sizeof(mapcache_source_gdal));
   if(!source) {
-    ctx->set_error(ctx, MAPCACHE_ALLOC_ERROR, "failed to allocate gdal source");
+    ctx->set_error(ctx, 500, "failed to allocate gdal source");
     return NULL;
   }
   mapcache_source_init(ctx, &(source->source));
   source->source.type = MAPCACHE_SOURCE_GDAL;
-  source->source.render_metatile = _mapcache_source_gdal_render_metatile;
+  source->source.render_map = _mapcache_source_gdal_render_metatile;
   source->source.configuration_check = _mapcache_source_gdal_configuration_check;
-  source->source.configuration_parse = _mapcache_source_gdal_configuration_parse;
+  source->source.configuration_parse_xml = _mapcache_source_gdal_configuration_parse;
   source->gdal_params = apr_table_make(ctx->pool,4);
+  GDALAllRegister();
   return (mapcache_source*)source;
 #else
   ctx->set_error(ctx, 400, "failed to create gdal source, GDAL support is not compiled in this version");
