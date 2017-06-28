c9cfcec4d838aa2f9fd2bd6ff0a39e466bdf0cca
Thomas Bonfort <thomas.bonfort@gmail.com>
2017-06-28 17:24:15 +0200

    [Backport branch-1-6] clear errors after failed dimension check when used in context of forwarding rules (#168)
    (cherry picked from commit 1917e889c5cf424ebb7368b9b41ffac38c91cde8)

--- lib/service_wms.c
+++ lib/service_wms.c
@@ -931,6 +931,7 @@ proxies:
         const char *value = apr_table_get(params,match_param->name);
         if(!value || match_param->_get_entries_for_value(ctx,match_param,value,NULL,NULL,NULL)->nelts == 0) {
           /* the parameter was not supplied, or did not validate: we don't apply this rule */
+          ctx->clear_errors(ctx);
           got_a_match = 0;
           break;
         }
