83c09332931cbec3b18eca1e0c7c0ad7e302a5ce
Māris Nartišs <maris.gis@gmail.com>
2024-01-22 14:05:01 +0200

    Allow to provide custom Cache-Control header values (fixes #322)

--- include/mapcache.h
+++ include/mapcache.h
@@ -1199,6 +1199,14 @@ struct mapcache_tileset {
    */
   int auto_expire;
 
+  /**
+   * Cache-Control directives to be set for a TMS response
+   *
+   * complements expiration set by the #expires parameter.
+   * \sa expires
+   */
+  const char *cache_control;
+
   int read_only;
   int subdimension_read_only;
 
