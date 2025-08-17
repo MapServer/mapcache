cf2a9436f64c46e131038d53eb8dbe8cb713c0ce
yawaaaa <github1369@gmail.com>
2025-08-17 17:25:15 +0800

    Delete scripts/vagrant directory

--- scripts/vagrant/packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-apt-get install -y cmake apache2
-apt-get install -y libcurl4-openssl-dev apache2-dev
-apt-get install -y libgdal-dev libfcgi-dev libpixman-1-dev gdal-bin libxml2-utils
-
